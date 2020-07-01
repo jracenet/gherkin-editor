0683506e93c3278bc094ceb7f1bcabb91012f46c
Joan Racenet <joan.racenet@hiptest.net>
2020-07-01 16:03:23 +0200

    Updates

---
+++ precache-manifest.36574ca5dc4945b4ea7c200527c82946.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f7a582a62b3204724f72bd00e7c9615",
+    "url": "/gherkin-editor/index.html"
+  },
+  {
+    "revision": "b17e03495eef424b848f",
+    "url": "/gherkin-editor/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e0416d8e08d47e790056",
+    "url": "/gherkin-editor/static/js/2.54a1114c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/gherkin-editor/static/js/2.54a1114c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b17e03495eef424b848f",
+    "url": "/gherkin-editor/static/js/main.39032c79.chunk.js"
+  },
+  {
+    "revision": "966310e41c67b841bf1c",
+    "url": "/gherkin-editor/static/js/runtime-main.6b6abc6b.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/gherkin-editor/precache-manifest.36574ca5dc4945b4ea7c200527c82946.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gherkin-editor/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
