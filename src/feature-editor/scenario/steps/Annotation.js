1fc0eb8ef41f653b326ef4672631794db09c36f1
Joan Racenet <joan.racenet@hiptest.net>
2020-07-01 17:02:41 +0200

    Reset annotation if it is empty

--- src/feature-editor/scenario/steps/Annotation.js
+++ src/feature-editor/scenario/steps/Annotation.js
@@ -5,24 +5,41 @@ export default class Annotation extends React.Component {
     super(props)
 
     this.handleChange = this.handleChange.bind(this)
+    this.state = {
+      value: props.annotation.slice().trim()
+    }
   }
 
   handleChange(e) {
-    this.props.onAnnotationChange(e.target.value)
+    let newAnnotation = e.target.value
+
+    if (newAnnotation.trim() === "") {
+      this.setState({
+        value: this.props.annotation.slice().trim()
+      })
+      return
+    }
+
+    this.setState({
+      value: newAnnotation
+    })
+
+    this.props.onAnnotationChange(newAnnotation)
   }
 
   render() {
     return (
       <span>
-        <input type="text" defaultValue={this.props.annotation.trim()}
+        <input type="text" value={this.state.value}
         name="annotation"
-        list="annotations" onBlur={this.handleChange}/>
+        list="annotations" onChange={e => this.setState({ value: e.target.value })} onBlur={this.handleChange}/>
 
         <datalist id="annotations">
           <select>
             <option value="Given"/>
             <option value="When"/>
             <option value="And"/>
+            <option value="Then"/>
           </select>
         </datalist>
       </span>
