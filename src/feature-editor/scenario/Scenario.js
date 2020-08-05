4636447f0a8cb4333b3de6c911bb8c33960dd101
Joan Racenet <joan.racenet@hiptest.net>
2020-08-05 11:32:47 +0200

    Refactor scenario class component into a function one

--- src/feature-editor/scenario/Scenario.js
+++ src/feature-editor/scenario/Scenario.js
@@ -2,68 +2,55 @@ import React from 'react'
 import ScenarioStep from './steps/Step'
 import GherkinAstMutator from 'lib/GherkinAstMutator'
 
-export default class Scenario extends React.Component {
-  constructor(props) {
-    super(props)
-    this.onEditScenarioName = this.editScenarioName.bind(this)
-    this.onEditStep = this.editStep.bind(this)
-    this.addStep = this.addStep.bind(this)
-    this.onEditAndAddStep = this.editAndAddStep.bind(this)
+export default function(props) {
+  function addStep(index) {
+    let newScenarioAst = GherkinAstMutator.addStepAt(props.scenario, index)
+
+    props.updateFeatureChild(newScenarioAst, props.index)
+
+    return newScenarioAst
   }
 
-  render() {
-    return (
-      <li class="tile">
-        {this.scenarioHeader()}
-        <ul class="steps-list">
-          {this.scenarioSteps()}
-          <button class="btn--secondary" onClick={this.addStep}>Add step</button>
-        </ul>
-      </li>
-    )
-  }
-
-  scenarioHeader() {
+  function scenarioHeader() {
+    function editScenarioName(e) {
+      let newScenarioAst = {...props.scenario}
+      newScenarioAst.name = e.target.value
+      props.updateFeatureChild(newScenarioAst, props.index)
+    }
     return <h2 class="editable-title">
-      <span class="gherkin-keyword">{this.props.scenario.keyword}:</span>
-      <input placeholder="Empty name" defaultValue={this.props.scenario.name} onBlur={this.onEditScenarioName}/>
-      <button class="btn--secondary" onClick={() => this.props.onDeleteScenario(this.props.index)}>Delete</button>
+      <span class="gherkin-keyword">{props.scenario.keyword}:</span>
+      <input placeholder="Empty name" defaultValue={props.scenario.name} onBlur={ editScenarioName }/>
+      <button class="btn--secondary" onClick={() => props.onDeleteScenario(props.index)}>Delete</button>
     </h2>
   }
 
-  scenarioSteps() {
-    return this.props.scenario.steps.map((step, index) =>
-      <ScenarioStep key={step.id} step={step} index={index} onEditStep={this.onEditStep} onAddStep={this.addStep} onEditAndAddStep={this.onEditAndAddStep}/>
+  function scenarioSteps() {
+    function editStep(stepAst, index) {
+      let newScenarioAst = {...props.scenario}
+      newScenarioAst.steps[index] = stepAst
+      props.updateFeatureChild(newScenarioAst, props.index)
+    }
+
+    function editAndAddStep(newStepAst, index) {
+      let newScenarioAst = {...props.scenario}
+      newScenarioAst.steps[index] = newStepAst
+      newScenarioAst = GherkinAstMutator.addStepAt(newScenarioAst, index)
+
+      props.updateFeatureChild(newScenarioAst, props.index)
+
+      return newScenarioAst
+    }
+
+    return props.scenario.steps.map((step, index) =>
+      <ScenarioStep key={step.id} step={step} index={index} onEditStep={editStep} onAddStep={addStep} onEditAndAddStep={editAndAddStep}/>
     )
   }
 
-  editScenarioName(e) {
-    let newScenarioAst = Object.assign(this.props.scenario)
-    newScenarioAst.name = e.target.value
-    this.props.updateFeatureChild(newScenarioAst, this.props.index)
-  }
-
-  editStep(stepAst, index) {
-    let newScenarioAst = Object.assign(this.props.scenario)
-    newScenarioAst.steps[index] = stepAst
-    this.props.updateFeatureChild(newScenarioAst, this.props.index)
-  }
-
-  addStep(index) {
-    let newScenarioAst = GherkinAstMutator.addStepAt(this.props.scenario, index)
-
-    this.props.updateFeatureChild(newScenarioAst, this.props.index)
-
-    return newScenarioAst
-  }
-
-  editAndAddStep(newStepAst, index) {
-    let newScenarioAst = Object.assign(this.props.scenario)
-    newScenarioAst.steps[index] = newStepAst
-    newScenarioAst = GherkinAstMutator.addStepAt(newScenarioAst, index)
-
-    this.props.updateFeatureChild(newScenarioAst, this.props.index)
-
-    return newScenarioAst
-  }
+  return <li class="tile">
+        { scenarioHeader() }
+        <ul class="steps-list">
+          { scenarioSteps() }
+          <button class="btn--secondary" onClick={ addStep }>Add step</button>
+        </ul>
+      </li>
 }
