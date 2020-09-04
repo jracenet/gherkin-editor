01cd2fe9eab6d54e3c8202ff455742166011a235
Joan Racenet <joan.racenet@hiptest.net>
2020-09-04 15:36:58 +0200

    Can add a row in an example

--- src/feature-editor/examples/ExampleTableRender.js
+++ src/feature-editor/examples/ExampleTableRender.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import DatatableRowContainer from '../datatable/DatatableRowContainer'
+import { IdGenerator } from '@cucumber/messages'
 
 export default function({example, exampleIndex, onEditExample}) {
 
@@ -15,10 +16,33 @@ export default function({example, exampleIndex, onEditExample}) {
     onEditExample(mutableExampleAST, exampleIndex)
   }
 
+  function addExampleRow() {
+    let newRowLength = example.tableHeader.cells.length
+    let newRow = {
+      id: IdGenerator.uuid(),
+      cells: []
+    }
+
+    for (let cellIndex = 0; cellIndex < newRowLength; cellIndex++) {
+      newRow.cells.push({
+        value: ""
+      })
+    }
+
+    console.log(example)
+    example.tableBody.push(newRow)
+    onEditExample(example, exampleIndex)
+  }
+
   return <table>
-    <thead><tr><DatatableRowContainer row={ example.tableHeader } index={ 0 } editRow={ editExampleHeader }/></tr></thead>
+    <thead><tr><DatatableRowContainer row={ example.tableHeader } editRow={ editExampleHeader }/></tr></thead>
     <tbody>{example.tableBody.map((row, index) => {
       return <tr><DatatableRowContainer row={ row } index={ index } editRow={ editExampleRow }/></tr>
     })}</tbody>
+     <tfoot>
+      <tr>
+        <td><button className="btn--secondary" onClick={ addExampleRow }>Add row</button></td>
+      </tr>
+    </tfoot>
   </table>
 }
